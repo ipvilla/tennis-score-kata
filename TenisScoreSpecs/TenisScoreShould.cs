a344339ca15c13af90626cdcbd1399912220a918
Isa Pérez <isperez@aidacanarias.com>
2022-05-10 18:36:36 +0200

    Initial commit

---
+++ TenisScoreSpecs/TenisScoreShould.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace TenisScoreSpecs
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
