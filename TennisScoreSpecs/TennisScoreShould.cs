086f7e982d5f0e32fc0a20ccd1cd085215174819
Isa Pérez <isperez@aidacanarias.com>
2022-05-16 10:40:33 +0200

    🧪 New test increase_player_2_score_to_15_when_player_2_scores_one_point

--- TennisScoreSpecs/TennisScoreShould.cs
+++ TennisScoreSpecs/TennisScoreShould.cs
@@ -27,11 +27,24 @@ namespace TennisScoreSpecs
 
             printer.Received().Print("Current score: 15 - love");
         }
+
+        [Test]
+        public void increase_player_2_score_to_15_when_player_2_scores_one_point()
+        {
+            var printer = Substitute.For<IScorePrinter>();
+            var tennisGame = new TennisGame(printer);
+            tennisGame.PlayerTwoScores();
+
+            tennisGame.PrintScore();
+
+            printer.Received().Print("Current score: love - 15");
+        }
     }
 
     public class TennisGame
     {
         private readonly IScorePrinter printer;
+        private int playerOneScore;
 
         public TennisGame(IScorePrinter printer)
         {
@@ -40,10 +53,22 @@ namespace TennisScoreSpecs
 
         public void PrintScore()
         {
-            printer.Print("Current score: love - love");
+            if (playerOneScore == 1)
+            {
+                printer.Print("Current score: 15 - love");
+            }
+            else
+            {
+                printer.Print("Current score: love - love");
+            }
         }
 
         public void PlayerOneScores()
+        {
+            playerOneScore++;
+        }
+
+        public void PlayerTwoScores()
         {
         }
     }
